4fb3eac9b3c7be61ea800e6ac1ece6a773167efc
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-01-20 11:14:27 +0000

    Rectifying

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ y#1
 st = "exactly"
 ln = len(st)
 pt = "#"
-st_1 = ""
-st_2 = ""
+st_1 = 0
+st_2 = 00
 for i in range(0,ln):
   ch = st[i]
   if (ch=="x"):
-    st_1 = ch + pt + str(i)
+    st_1 = st_1 + 1
   elif (ch=="y"):
-    st_2 = ch + pt + str(i)
-print(st_1)
-print(st_2)
+    st_2 = st_2 + 1
+print(f"x#{st_1}")
+print(f"y#{st_2}")
+
